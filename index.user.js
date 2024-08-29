2f255abc30e2ca16ccc72f4d25f8bf8e5e83f573
flynncao <flynncao@yahoo.com>
2024-08-29 21:23:14 +0800

    init

---
+++ index.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name  
+// @version 
+// @description 
+// @author Flynn Cao
+// @namespace 
+// @match  
+// @include 
+// @license MIT
+// ==/UserScript==
+
+;(async function () {
+                     console.log('Welcome!')
+})()
